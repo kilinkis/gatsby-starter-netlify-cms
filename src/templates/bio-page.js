1964994a51c07a7a9a0213c2411db438a11ab343
Juan Manuel Incaurgarat <juanofx@gmail.com>
2018-10-08 13:59:59 +0200

    same

---
+++ src/templates/bio-page.js
@@ -0,0 +1,82 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+import Img from "gatsby-image";
+
+export const AboutPageTemplate = ({
+  title,
+  content,
+  contentComponent,
+  image
+}) => {
+  const PageContent = contentComponent || Content;
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <Img fixed={image.childImageSharp.fixed} />
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+AboutPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func
+};
+
+const AboutPage = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  //console.log("data " + JSON.stringify(data.file));
+
+  return (
+    <Layout>
+      <AboutPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        content={post.html}
+        image={data.file}
+      />
+    </Layout>
+  );
+};
+
+AboutPage.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export default AboutPage;
+
+export const aboutPageQuery = graphql`
+  query AboutPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+    file(relativePath: { eq: "profile-pic-cropped-min.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 125, height: 125) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`;
